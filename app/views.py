ef3addf5f10926203142d4b043690d2882f5da1f
Nazareno Duarte <dantegamer680@gmail.com>
2024-11-29 19:32:08 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -13,7 +13,8 @@ def index_page(request):
 def home(request):
     images = []
     favourite_list = []
-
+    images=services.getAllImages()
+    
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
 def search(request):
